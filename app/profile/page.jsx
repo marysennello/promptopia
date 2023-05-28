35450879e19fe7e16b17cf5ae81752461362d94d
Mary Sennello <marysenn@uw.edu>
2023-05-28 16:13:42 -0700

    added edit and delete functionality for prompt creator

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -29,6 +29,19 @@ const MyProfile = () => {
 
   }
   const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are yous ure you want to delete this prompt?");
+    if(hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE'
+        })
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
   }
 
